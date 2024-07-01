7a1663b715f468d79f8cb6921a608bc84c8f7f58
DEBLAND <hoseinnysyan1385@gmail.com>
2024-07-01 14:18:42 +0330

    after testing my account changes

--- api_test.py
+++ api_test.py
@@ -1,5 +1,6 @@
 from sql_files import sql_tweet_functions
 from datetime import datetime
+
 date_and_time = datetime.now()
 instance_for_test = sql_tweet_functions.SqlFunctions(tweet_channel="@dailymonitor", tweet_id="21312", tweet_title="salam", used_comment="working on project", tweet_link="https://x.com/salam/132132132", comment_post_datetime=f"{date_and_time}")
 test = instance_for_test.update_data()
--- sql_commands.py
+++ sql_commands.py
@@ -3,7 +3,7 @@ import sqlite3 as sql
 connect = sql.connect('acatweegram.db')
 cursor = connect.cursor()
 
-command = f'SELECT tweet_id FROM tweet_data WHERE tweet_channel = "@dailymonitor"'
+command = f'INSERT INTO tweet_data(ID, tweet_channel) VALUES (4, "@acatweegram")'
 tweet_data = cursor.execute(command)
-data = tweet_data.fetchall()[0][0]
-print(data)
+connect.commit()
+
