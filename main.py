b0acb356afeb79e4d1277afd5478e9738cd8b229
DEBLAND <hoseinnysyan1385@gmail.com>
2024-07-01 21:44:45 +0330

    create add channel and view before it section

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from telegram import *
 from telegram.ext import *
 from sql_files.sql_tweet_functions import *
 from sql_files.sql_admin_functions import *
+from sql_files.add_channel import *
+from sql_files.remove_channel import *
+from sql_files.check_database import *
 from tweepy import *
 from comments.comments import *
 from tweepy.asynchronous import *
@@ -101,13 +104,19 @@ if you click on change notify method and click on No we don`t send you notificat
 
     # you can add your desire tweeter user channel to get data from and automatically send comment
     elif "🟢 add your desire channel" in update.message.text:
+        instance_of_channels = SqlShowAndInsert()
+        data = instance_of_channels.check_database()
+        list = []
+        for da in data:
+            list.append(da)
         await context.bot.send_message(update.effective_user.id, f"""
-if you want to add channel to check it also you have to add name of channel starting with "@" so please leave channel name
+if you want to add channel to check it also you have to add name of channel starting with "@" so please leave channel name\n\n
+now_channels = {list}
         """)
         return ADDING_CHANNEL
 
     # you can delete your desire channel to prevent sending comment and data
-    elif "🔴 delete channel" in update.message.text:
+    elif "🔴 delete Channel" in update.message.text:
         await context.bot.send_message(update.effective_user.id, f"""
 if you want to delete channel that you added please insert the name below
         """)
@@ -133,6 +142,8 @@ async def add_channel(update: Update, context: CallbackContext) -> None:
     channel_name = update.message.text
     if channel_name.startswith('@'):
         print(channel_name)
+        instance_of_show_and_insert = SqlShowAndInsert(channel_name)
+        instance_of_show_and_insert.Insert_channel()
         # Process the channel name (e.g., store it, start monitoring it, etc.)
         await context.bot.send_message(update.effective_user.id, f"Channel '{channel_name}' has been added and will be monitored.")
     else:
@@ -171,102 +182,102 @@ async def get_call_back_data(update: Update, context: CallbackContext) -> None:
     print(query.data)
 
 
-async def get_user_tweets():
-    for user_name, user_id in zip(Accounts.accounts, Accounts.accounts_id_ordered):
-        url = "https://twitter154.p.rapidapi.com/user/tweets"
-
-        # parameters that we need to call url with
-        querystring = {
-            "username": user_name,
-            "limit": "1",
-            "user_id": user_id,
-            "include_replies": False,
-            "include_pinned": False
-        }
-
-        headers = {
-            "x-rapidapi-key": "cb55117503mshb4d680ddb2c3067p1364dejsn60b23ba912e6",
-            "x-rapidapi-host": "twitter154.p.rapidapi.com"
-        }
-
-        # # send request by get method and get response
-        response = re.get(url, headers=headers, params=querystring)
-
-        # get and extract data from response
-        data = response.json()
-        tweet_id = data['results'][0]['tweet_id']
-        tweet_title = data['results'][0]['text']
-        channel_name = data['results'][0]['user']['username']
-        random_comment_text = random_comment()
-        print(f"random_comment_result {random_comment_text}")
-
-        # in here we will get instance of class sql function and then check the new tweet then run functions
-        print(f"this is user name of loop {user_name}")
-        # there are instance of sql function to run method of inside it
-        check_data_equality = SqlFunctions(tweet_channel=f'{user_name}', tweet_id=f'{tweet_id}')
-        is_equal = check_data_equality.Is_tweet_data_equal()
-        # this function will get inputs and save them or update them and then send comment
-        if is_equal:
-            print('data are equal')
-            pass
-        else:
-            try:
-                # if data is not equal it mean there are new post so it will send comment and change the row data
-                tweet_link = comment_post.send_comment(f'{random_comment_text}', post_id=f'{tweet_id}')
-                comment_post_date_time = datetime.datetime.now()
-                # make instance of sql functions and save data below it
-                sql_update_instance = SqlFunctions(tweet_channel=f'{channel_name}', tweet_id=f'{tweet_id}', tweet_title=f'{tweet_title}', used_comment=f'{random_comment_text}', tweet_link=f"{tweet_link}", comment_post_datetime=f'{comment_post_date_time}')
-                save_data = sql_update_instance.update_data_or_insert()
-                # in this section we will run command to send message to all admins about this happening
-
-                # this is instance of function that for each id inside admin table it will send message
-                sql_admin_instance = AdminSql()
-                data = sql_admin_instance.send_all_admin_ids()
-                if save_data:
-                    print(f"new row updated from {channel_name} and new dataset has been added")
-                    logging.info(msg=f"new row updated from {channel_name} and new dataset has been added")
-                else:
-                    logging.debug(msg=f"there is problem with adding data to database")
-                keyboards = [
-                    [InlineKeyboardButton('go to tweet page 🔗', url=tweet_link)],
-                ]
-                reply_markup_keyboard = InlineKeyboardMarkup(keyboards, )
-                for id in data:
-                    await bot.send_message(chat_id=f"{id[0]}", text=f"""
-Hi user: {id[1]} 🌟 
-I`ve sent this message:``{random_comment_text}``\n\n to tweet name: {tweet_title} 😉
-                \n
-to channel: {user_name}                
-
-and tweet id was: 🔢 {tweet_id}
-\n
-date & time: {comment_post_date_time}
-""", disable_web_page_preview=True, reply_markup=reply_markup_keyboard)
-                # if user in it`s setting turn email sending true we can send user notification from email also
-                if id[2]:
-                    user_email_sending_of_tweets_data(user_name=f"{id[1]}", channel_name=f"{channel_name}", email=f"{id[3]}", random_comment_text=f"{random_comment_text}", tweet_title=f'{tweet_title}', tweet_id=f"{tweet_id}")
-                    if user_email_sending_of_tweets_data:
-                        await bot.send_message(chat_id=f"{id[0]}", text=f"we`ve sent you the email address because you gave us that permission 📧", disable_web_page_preview=True)
-                    else:
-                        await bot.send_message(chat_id=f"{id[0]}", text=f"we can`t send you email notification that`s may because you ent us wrong email address")
-                else:
-                    pass
-            except:
-                logging.error(msg='can`t send message may it`s repetitive')
-
-
-async def run_forever():
-    while True:
-        await get_user_tweets()
-        await asyncio.sleep(1 * 60)  # Adjust the sleep time as needed to control the frequency of the requests
-
-
-# this section will monitoring the data from sources that we need to know about themselves posts and then will comment randomly under their posts
-# after that it will let admin know and send link to admin beside the all data of that posts of pages it should be very fast and avoid spaming a lot
-# because my it block our bot and our services
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_forever())
+# async def get_user_tweets():
+#     for user_name, user_id in zip(Accounts.accounts, Accounts.accounts_id_ordered):
+#         url = "https://twitter154.p.rapidapi.com/user/tweets"
+#
+#         # parameters that we need to call url with
+#         querystring = {
+#             "username": user_name,
+#             "limit": "1",
+#             "user_id": user_id,
+#             "include_replies": False,
+#             "include_pinned": False
+#         }
+#
+#         headers = {
+#             "x-rapidapi-key": "cb55117503mshb4d680ddb2c3067p1364dejsn60b23ba912e6",
+#             "x-rapidapi-host": "twitter154.p.rapidapi.com"
+#         }
+#
+#         # # send request by get method and get response
+#         response = re.get(url, headers=headers, params=querystring)
+#
+#         # get and extract data from response
+#         data = response.json()
+#         tweet_id = data['results'][0]['tweet_id']
+#         tweet_title = data['results'][0]['text']
+#         channel_name = data['results'][0]['user']['username']
+#         random_comment_text = random_comment()
+#         print(f"random_comment_result {random_comment_text}")
+#
+#         # in here we will get instance of class sql function and then check the new tweet then run functions
+#         print(f"this is user name of loop {user_name}")
+#         # there are instance of sql function to run method of inside it
+#         check_data_equality = SqlFunctions(tweet_channel=f'{user_name}', tweet_id=f'{tweet_id}')
+#         is_equal = check_data_equality.Is_tweet_data_equal()
+#         # this function will get inputs and save them or update them and then send comment
+#         if is_equal:
+#             print('data are equal')
+#             pass
+#         else:
+#             try:
+#                 # if data is not equal it mean there are new post so it will send comment and change the row data
+#                 tweet_link = comment_post.send_comment(f'{random_comment_text}', post_id=f'{tweet_id}')
+#                 comment_post_date_time = datetime.datetime.now()
+#                 # make instance of sql functions and save data below it
+#                 sql_update_instance = SqlFunctions(tweet_channel=f'{channel_name}', tweet_id=f'{tweet_id}', tweet_title=f'{tweet_title}', used_comment=f'{random_comment_text}', tweet_link=f'{tweet_link}', comment_post_datetime=f'{comment_post_date_time}')
+#                 save_data = sql_update_instance.update_data_or_insert()
+#                 # in this section we will run command to send message to all admins about this happening
+#
+#                 # this is instance of function that for each id inside admin table it will send message
+#                 sql_admin_instance = AdminSql()
+#                 data = sql_admin_instance.send_all_admin_ids()
+#                 if save_data:
+#                     print(f"new row updated from {channel_name} and new dataset has been added")
+#                     logging.info(msg=f"new row updated from {channel_name} and new dataset has been added")
+#                 else:
+#                     logging.debug(msg=f"there is problem with adding data to database")
+#                 keyboards = [
+#                     [InlineKeyboardButton('go to tweet page 🔗', url=tweet_link)],
+#                 ]
+#                 reply_markup_keyboard = InlineKeyboardMarkup(keyboards, )
+#                 for id in data:
+#                     await bot.send_message(chat_id=f"{id[0]}", text=f"""
+# Hi user: {id[1]} 🌟
+# I`ve sent this message:``{random_comment_text}``\n\n to tweet name: {tweet_title} 😉
+#                 \n
+# to channel: {user_name}
+#
+# and tweet id was: 🔢 {tweet_id}
+# \n
+# date & time: {comment_post_date_time}
+# """, disable_web_page_preview=True, reply_markup=reply_markup_keyboard)
+#                 # if user in it`s setting turn email sending true we can send user notification from email also
+#                 if id[2]:
+#                     user_email_sending_of_tweets_data(user_name=f"{id[1]}", channel_name=f"{channel_name}", email=f"{id[3]}", random_comment_text=f"{random_comment_text}", tweet_title=f'{tweet_title}', tweet_id=f"{tweet_id}")
+#                     if user_email_sending_of_tweets_data:
+#                         await bot.send_message(chat_id=f"{id[0]}", text=f"we`ve sent you the email address because you gave us that permission 📧", disable_web_page_preview=True)
+#                     else:
+#                         await bot.send_message(chat_id=f"{id[0]}", text=f"we can`t send you email notification that`s may because you ent us wrong email address")
+#                 else:
+#                     pass
+#             except:
+#                 logging.error(msg='can`t send message may it`s repetitive')
+#
+#
+# async def run_forever():
+#     while True:
+#         await get_user_tweets()
+#         await asyncio.sleep(5 * 60)  # Adjust the sleep time as needed to control the frequency of the requests
+#
+#
+# # this section will monitoring the data from sources that we need to know about themselves posts and then will comment randomly under their posts
+# # after that it will let admin know and send link to admin beside the all data of that posts of pages it should be very fast and avoid spaming a lot
+# # because my it block our bot and our services
+# if __name__ == "__main__":
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(run_forever())
 
 
 # Create the application and pass it your bot's token
